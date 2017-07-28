11030c5e0f41f8038664670f7c5e408458cc6b61
augustus-rex <august.lindberg@hotmail.com>
2017-07-28 10:54:05 +0200

    Fix broken paths

--- loop_ticking_sound.sh
+++ loop_ticking_sound.sh
@@ -2,6 +2,8 @@
 # Loop the pomodoro ticking noise. The ticking soundfile is only about 15
 # minutes long, so without a loop it won't last a whole pomodoro.
 
+script_dir="$(dirname "$(readlink -f "$0")")"
+
 while [ 1 ]; do
-	paplay ./sounds/pomodoroclock_ticking.ogg
+	paplay $script_dir/sounds/pomodoroclock_ticking.ogg
 done
